759c8cd773ba25b7f633d50a0104d1fe2801cf08
Kai Meikle <kaimeikle123@gmail.com>
2022-05-03 00:18:09 +0000

    complete exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, length: { in: 40..200 }
+  validates :belongs_to, presence: true
+end
+
+class Store < ActiveRecord::Base
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, length: { minimum: 1 }
+end
+
+store_name = $stdin.gets.chomp
+new_store = Store.create(name: store_name)
+new_store.errors.full_messages.each do |msg|
+  puts msg
+end
